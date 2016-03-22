c058680780aa918bb3ded9c94337c964b3312c72
Jade-L <lemaissi.jade@gmail.com>
2016-03-22 08:15:28 +0100

    installation gem + config, change routes, start html body

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :projects
   get 'static_pages/index'
+  get 'static_pages/load_desc'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
